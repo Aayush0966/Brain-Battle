a32628be9858d0fb9264c975f2b98694ce742cbd
Aayush0966 <aayushx699@gmail.com>
2024-12-22 23:15:01 +0545

    Add tailwind-scrollbar package and update layout styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,7 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate"),
-    require('tailwind-scrollbar-hide')
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar')
   ],
 }
